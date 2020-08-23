ab21c7351bd245912a80e9de6f3a8fe3d62c3bcd
Matthew Flinders <sodaime@gmail.com>
2020-08-23 12:56:26 +0100

    first commit

---
+++ main.cc
@@ -0,0 +1,50 @@
+// Create a single-colored bitmap
+#include <iostream>
+#include "palette.h"
+using namespace std;
+
+// Height and width in pixels
+const int h = 1081;
+const int w = 1919;
+
+void pixel_color(int* col, int y, int x)
+{
+  // fill col with rgb pixel color
+  col[0] = int(float(x)/float(w)*255.99); //R
+  col[1] = int(float(y)/float(h)*255.99); //G
+  col[2] = 51; //B
+}
+
+void shade_buffer(char* pixel_buffer)
+{
+  int col[3];
+
+  for (int r=0; r<h; r++)
+  {
+    for (int c=0; c<w; c++)
+    {
+
+      // Calculate the color for pixel (r, c)
+      pixel_color(col, r, c);
+
+      // Copy the pixel color to the buffer
+      pixel_buffer[r*w*3 + c*3 + 0] = col[0]; // R
+      pixel_buffer[r*w*3 + c*3 + 1] = col[1]; // G
+      pixel_buffer[r*w*3 + c*3 + 2] = col[2]; // B
+    }
+  }
+}
+
+int main(int argc, char* argv[])
+{
+
+  char *pixel_buffer = new char[w*h*3];
+
+  cout << "Shading pixel buffer\n";
+  shade_buffer(pixel_buffer);
+
+  cout << "Writing to file\n";
+  bmp_write(pixel_buffer, h, w, "my_palette.bmp");
+
+  return 0;
+}
---
+++ palette.cc
@@ -0,0 +1,179 @@
+#include <fstream>
+#include <iostream>
+#include <windows.h>
+#include "palette.h"
+using namespace std;
+
+const int HEADER_SIZE = 54;
+
+void win_write(char* msg, DWORD msg_len, HANDLE file_handle)
+{
+  BOOL error_flag = FALSE;
+
+  DWORD bytes_written = 0;
+  error_flag = WriteFile(
+		  file_handle,
+		  msg,
+		  msg_len,
+		  &bytes_written,
+		  NULL);
+
+  if (error_flag == FALSE)
+  {
+	  cout << "Unable to write to file.";
+	  CloseHandle(file_handle);
+	  exit(1);
+  }
+  else
+  {
+	  if (bytes_written != msg_len)
+	  {
+		  cout << "Write failed to complete: " << bytes_written << " bytes written.";
+		  CloseHandle(file_handle);
+		  exit(1);
+	  }
+  }
+}
+
+char* generate_header(int filesize, int w, int h)
+{
+  static char header[HEADER_SIZE];
+
+  // 'BM' to identify file as BMP
+  // could use memcpy
+  header[0] = 'B';
+  header[1] = 'M';
+
+  // Little-endian filesize in bytes
+  write_int_to_header(header, HEADER_SIZE, 2);
+
+  // 4 bytes of padding
+  write_int_to_header(header, 0, 6);
+
+  // Data start address
+  write_int_to_header(header, HEADER_SIZE, 10);
+
+  // Size of BITMAPINFOHEADER (40 bytes)
+  write_int_to_header(header, 40, 14);
+
+  // Bitmap width in pixels
+  write_int_to_header(header, w, 18);
+
+  // Bitmap height in pixels
+  write_int_to_header(header, h, 22);
+
+  // Necessary stuff which is independent of data
+  header[26] = 0x1;
+  header[27] = 0x0;
+
+  // Bits per pixel (2 bytes)
+  header[28] = 0x18;
+  header[29] = 0x0;
+
+  // Compression method (0 = no compression)
+  write_int_to_header(header, 0, 30);
+
+  // Padding (assuming no compression)
+  write_int_to_header(header, 0, 34);
+
+  // Paddingggg
+  write_int_to_header(header, 0, 38);
+  write_int_to_header(header, 0, 42);
+  write_int_to_header(header, 0, 46);
+  write_int_to_header(header, 0, 50);
+
+  return header;
+}
+
+// Write a 4-byte word to the header. Little-endian
+void write_int_to_header(char* header, int number, int start)
+{
+
+  int mask = 0x000000ff;
+
+  for (int i=0; i < 4; i++)
+  {
+    header[i+start] = (char)((number & mask) >> (8*i));
+    mask = mask << 8;
+  }
+
+}
+
+void buffer_to_data(char* pixel_buffer, int h, int w, char* data, int pad_len, int byte_w)
+{
+
+  for (int row=0; row<h; row++)
+  {
+
+    for (int col=0; col<w; col++)
+    {
+      data[row*byte_w + col*3 + 0] =  pixel_buffer[row*w*3 + col*3 + 2]; // B
+      data[row*byte_w + col*3 + 1] =  pixel_buffer[row*w*3 + col*3 + 1]; // G
+      data[row*byte_w + col*3 + 2] =  pixel_buffer[row*w*3 + col*3 + 0]; // R
+    }
+
+    // Pad the number of bytes in the row to a multiple of 4
+    for (int pad=0; pad<pad_len; pad++)
+    {
+      data[row*byte_w + w*3 + pad] = char(0x01); // Padding character is arbitrary
+    }
+
+  }
+}
+
+// Writes a length 3*h*w byte array to a bitmap of dimensions (h,w)
+void bmp_write(char* pixel_buffer, int h, int w, const char* filename)
+{
+  ofstream fout;
+
+  // Pixel buffer needs padding for 4 byte alignment
+  int pad_len = (4 - (3*w) % 4) % 4;
+  int byte_w = 3*w + pad_len;
+
+  cout << "Padding length: " << pad_len << "\n";
+
+  const int filesize = HEADER_SIZE + h * byte_w;
+
+  cout << "Generating header\n";
+  char* header = generate_header(filesize, w, h);
+
+  cout << "Allocating memory for bitmap data (" << h*byte_w << " bytes)\n";
+  // This will be too big (I think if h*byte_w is larger than the largest integer)
+  char *data = new char[h*byte_w];
+
+  //cout << "Copying pixel buffer to bitmap data\n";
+  buffer_to_data(pixel_buffer, h, w, data, pad_len, byte_w);
+
+  // Open file
+  cout << "Opening bitmap file" << filename << "\n";
+
+  HANDLE file_handle;
+
+  file_handle = CreateFile(filename, // Filename
+		  GENERIC_WRITE, // Open mode
+		  0, // Share mode. 0 to block access for other processes
+		  NULL, // Security attribute
+		  CREATE_ALWAYS, // Create disposition. CREATE_ALWAYS for create/overwrite
+		  FILE_ATTRIBUTE_NORMAL, // Windows file system attributes (hidden, archive etc.)
+		  NULL); // Can inherit file attributes from another file handle or NULL.
+
+  // Deal with failure
+
+  if (file_handle == INVALID_HANDLE_VALUE)
+  {
+    cout << "Unable to open file.";
+    exit(1);
+  }
+
+  cout << "Writing bitmap header\n";
+  win_write(header, HEADER_SIZE, file_handle);
+
+  cout << "Writing bitmap data\n";
+  win_write(data, filesize - HEADER_SIZE, file_handle);
+
+  cout << "Write finished. Closing "<< filename << "\n";
+  CloseHandle(file_handle);
+
+  delete[] data;
+
+}
