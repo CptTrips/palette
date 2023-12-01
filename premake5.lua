833b67cc08f10a166729fa0119961bcc8b9de0b3
Matt Flinders <sodaime@gmail.com>
2023-12-01 14:16:50 +0000

    Add premake build script

---
+++ premake5.lua
@@ -0,0 +1,38 @@
+workspace "palette"
+    configurations { "Debug", "Release" }
+    location "build"
+
+project "palette"
+    kind "StaticLib"
+    language "C"
+    targetdir "lib/%{cfg.buildcfg}"
+
+    files {"palette.h", "palette.cc"}
+
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        symbols "On"
+
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        optimize "On"
+
+
+project "DemoApp"
+    kind "ConsoleApp"
+    language "C"
+    targetdir "bin/%{cfg.buildcfg}"
+
+    files {"main.cc"}
+
+    links {"palette"}
+
+    libdirs {"lib/%{cfg.buildcfg}"}
+
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        symbols "On"
+
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        optimize "On"
